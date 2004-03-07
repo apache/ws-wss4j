6c28922c620ca0173622340aa9dfc186dcbf4b6f
Werner Dittmann <werner@apache.org>
2004-03-07 10:34:59 +0000

    Added a certificate into keystore to test DSA/SHA1 signature algo

--- keys/genKeystore.sh
+++ keys/genKeystore.sh
@@ -1,9 +1,9 @@
 #
 # Clean out the server and client keystores
-rm wss4j.keystore
+# rm wss4j.keystore
 
 # Generate the key that will be used for wss4j
 # (use security as the password)
-$JAVA_HOME/bin/keytool -genkey -alias wss4jCert -keyalg RSA -keystore wss4j.keystore -dname "CN=wernerd,OU=WSS4J,O=Apache,L=Munich,ST=Bayern,C=DE"
-
+# $JAVA_HOME/bin/keytool -genkey -alias wss4jCert -keyalg RSA -keystore wss4j.keystore -dname "CN=wernerd,OU=WSS4J,O=Apache,L=Munich,ST=Bayern,C=DE"
+$JAVA_HOME/bin/keytool -genkey -alias wss4jCertDSA -keystore wss4j.keystore -dname "CN=wernerdDSA,OU=WSS4J,O=Apache,L=Munich,ST=Bayern,C=DE"
 
--- keys/signConvertImportCert.sh
+++ keys/signConvertImportCert.sh
@@ -8,6 +8,7 @@ openssl x509 -outform DER -in cert.pem -out cert.crt
 # import the CA and server certificate into wss4j's keystore (note that 
 # importing the server certificate results in the keystore's wss4j 
 # certificate being updated with the new signature):
-$JAVA_HOME/bin/keytool -import -file ca.crt -keystore wss4j.keystore
-$JAVA_HOME/bin/keytool -import -alias wss4jCert -file cert.crt -keystore wss4j.keystore
+# $JAVA_HOME/bin/keytool -import -file ca.crt -keystore wss4j.keystore
+# $JAVA_HOME/bin/keytool -import -alias wss4jCert -file cert.crt -keystore wss4j.keystore
+$JAVA_HOME/bin/keytool -import -alias wss4jCertDSA -file cert.crt -keystore wss4j.keystore
 
