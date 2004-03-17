a3ecac1b043b44454887612a86a0356e5570191f
Werner Dittmann <werner@apache.org>
2004-03-17 19:36:30 +0000

    A todolist - feel free to add items - and to work on items.

---
+++ src/org/apache/ws/security/Todolist.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 17.03.2004
+ *
+ */
+package org.apache.ws.security;
+/**
+ * @author wdi
+ *
+ */
+public class Todolist {
+	/*
+	 * This is not really a java source but a todo list. Eclipse displays
+	 * this list automatically on the task list. Convenient.
+	 * 
+	 * Please fill in any new items that should be done. Rename the ToDo marker
+	 * to DONE if done, add your name, and some comments if necessary to keep the 
+	 * list uptodate.
+	 * 
+	 * TODO: Clean up exception handling, remap to WSSecurityException, avoid generic Exception.
+	 * TODO: make Cipher provider configurable
+	 * TODO: new function to perform Certificate handover from request to response according to Kojira's
+	 * 		 explanation
+	 * TODO: Better texts for the Exception, see OASIS WSS specs for recommended fault strings
+	 *  
+	 */
+
+}
