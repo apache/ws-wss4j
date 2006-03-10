8e6766f24ce212ab7c9f0733e53a0e3315cf00e7
Werner Dittmann <werner@apache.org>
2006-03-10 16:14:32 +0000

    Create new certificates.

--- keys/genKeystore.sh
+++ keys/genKeystore.sh
@@ -4,6 +4,6 @@
 
 # Generate the key that will be used for wss4j
 # (use security as the password)
-# $JAVA_HOME/bin/keytool -genkey -alias wss4jCert -keyalg RSA -keystore wss4j.keystore -dname "CN=wernerd,OU=WSS4J,O=Apache,L=Munich,ST=Bayern,C=DE"
-$JAVA_HOME/bin/keytool -genkey -alias wss4jCertDSA -keystore wss4j.keystore -dname "CN=wernerdDSA,OU=WSS4J,O=Apache,L=Munich,ST=Bayern,C=DE"
+"$JAVA_HOME/bin/keytool" -genkey -alias wss4jCert -keyalg RSA -keystore wss4j.keystore -dname "CN=werner,OU=WSS4J,O=Apache,L=Munich,ST=Bayern,C=DE"
+"$JAVA_HOME/bin/keytool" -genkey -alias wss4jCertDSA -keystore wss4j.keystore -dname "CN=wernerDSA,OU=WSS4J,O=Apache,L=Munich,ST=Bayern,C=DE"
 
