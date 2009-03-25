dce8a36118aea89b151dfec12296923f5aaa368f
Colm O hEigeartaigh <coheigea@apache.org>
2009-03-25 15:55:34 +0000

    Forgot to update PackageTests in the last merge.

--- test/wssec/PackageTests.java
+++ test/wssec/PackageTests.java
@@ -77,6 +77,7 @@ public class PackageTests extends TestCase {
         suite.addTestSuite(TestWSSecurityDataRef.class);
         suite.addTestSuite(TestWSSecurityDataRef1.class);
         suite.addTestSuite(TestWSSecurityCertError.class);
+        suite.addTestSuite(TestWSSecurityEncryptionParts.class);
         suite.addTestSuite(TestWSSecuritySignatureParts.class);
         suite.addTestSuite(TestWSSecurityUTSignature.class);
         suite.addTestSuite(TestWSSecurityWSS60.class);
