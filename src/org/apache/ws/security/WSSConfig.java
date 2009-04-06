a1604617b33e10064e9f4a2355188109affd4570
Colm O hEigeartaigh <coheigea@apache.org>
2009-04-06 14:06:25 +0000

    [WSS-99] - Adding BouncyCastle provider using addProvider rather than insertProviderAt(..., 2)  - The previous way of doing things where BouncyCastle was added as the second provider was preventing WSS4J being used out of the box on Solaris and AIX.  - Also removed JuiCE provider.

--- src/org/apache/ws/security/WSSConfig.java
+++ src/org/apache/ws/security/WSSConfig.java
@@ -264,12 +264,7 @@ public class WSSConfig {
         if (!staticallyInitialized) {
             org.apache.xml.security.Init.init();
             if (addJceProviders) {
-                /*
-                 * The last provider added has precedence, that is if JuiCE can be added
-                 * then WSS4J uses this provider.
-                 */
                 addJceProvider("BC", "org.bouncycastle.jce.provider.BouncyCastleProvider");
-                addJceProvider("JuiCE", "org.apache.security.juice.provider.JuiCEProviderOpenSSL");
             }
             Transform.init();
             try {
@@ -526,9 +521,9 @@ public class WSSConfig {
             if (java.security.Security.getProvider(id) == null) {
                 Class c = Loader.loadClass(className, false);
                 int ret = 
-                    java.security.Security.insertProviderAt(
-                        (java.security.Provider) c.newInstance(), 2
-                    );
+                        java.security.Security.addProvider(
+                            (java.security.Provider) c.newInstance()
+                        );
                 if (log.isDebugEnabled()) {
                     log.debug("The provider " + id + " was added at position: " + ret);
                 }                
