ec0b59b024b681e5bc55a6e5e006eaf36b13ecee
Colm O hEigeartaigh <coheigea@apache.org>
2009-08-10 10:13:07 +0000

    [WSS-205] - Changed all calls in WSS4JHandler from setProperty(..., null) to removeProperty

--- src/org/apache/ws/security/handler/WSS4JHandler.java
+++ src/org/apache/ws/security/handler/WSS4JHandler.java
@@ -211,7 +211,7 @@ public class WSS4JHandler extends WSHandler implements Handler {
         reqData.setUsername((String) getOption(WSHandlerConstants.USER));
         if (reqData.getUsername() == null || reqData.getUsername().equals("")) {
             reqData.setUsername((String) mc.getProperty(WSHandlerConstants.USER));
-            mc.setProperty(WSHandlerConstants.USER, null);
+            mc.removeProperty(WSHandlerConstants.USER);
         }
 
         /*
@@ -312,7 +312,7 @@ public class WSS4JHandler extends WSHandler implements Handler {
                 }
                 throw new JAXRPCException("Couldn't set content on SOAPPart" + se.getMessage(), se);
             }
-            mc.setProperty(WSHandlerConstants.SND_SECURITY, null);
+            mc.removeProperty(WSHandlerConstants.SND_SECURITY);
         }
         if (doDebug) {
             log.debug("WSS4JHandler: exit invoke()");
