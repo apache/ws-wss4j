5e466e61ffc0e5b665fe9cf21e4364aa7c7d722b
Colm O hEigeartaigh <coheigea@apache.org>
2010-05-26 10:41:11 +0000

    Added a test to programatically construct a PKIPATH BinarySecurityToken.

--- test/wssec/TestWSSecurityNew3.java
+++ test/wssec/TestWSSecurityNew3.java
@@ -193,6 +193,34 @@ public class TestWSSecurityNew3 extends TestCase implements CallbackHandler {
         verify(signedDoc);
     }
     
+    /**
+     * Test that signs and verifies a WS-Security envelope
+     * <p/>
+     * 
+     * @throws java.lang.Exception Thrown when there is any problem in signing or verification
+     */
+    public void testBSTPKIPathSignature() throws Exception {
+        WSSecSignature builder = new WSSecSignature();
+        builder.setUserInfo("wss40", "security");
+        builder.setKeyIdentifierType(WSConstants.BST_DIRECT_REFERENCE);
+        builder.setUseSingleCertificate(false);
+        LOG.info("Before Signing....");
+        Document doc = SOAPUtil.toSOAPPart(SOAPMSG);
+        WSSecHeader secHeader = new WSSecHeader();
+        secHeader.insertSecurityHeader(doc);
+        
+        Crypto pkiCrypto = CryptoFactory.getInstance("wss40.properties");
+        Document signedDoc = builder.build(doc, pkiCrypto, secHeader);
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("After PKI Signing....");
+            String outputString = 
+                org.apache.ws.security.util.XMLUtils.PrettyDocumentToString(signedDoc);
+            LOG.debug(outputString);
+        }
+        
+        secEngine.processSecurityHeader(doc, null, this, pkiCrypto, null);
+    }
     
     /**
      * Test that signs and verifies a WS-Security envelope
