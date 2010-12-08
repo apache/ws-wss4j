ad0a35f8f984eb56f05d9459cc9cd600d8ea8745
Colm O hEigeartaigh <coheigea@apache.org>
2010-12-08 16:32:42 +0000

    [WSS-250] - ...and the missing handlers from the last commit.

---
+++ src/test/java/org/apache/ws/security/common/UsernamePasswordCallbackHandler.java
@@ -0,0 +1,71 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.ws.security.common;
+
+import org.apache.ws.security.WSPasswordCallback;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A Callback Handler implementation for the case of processing a Username Token.
+ */
+public class UsernamePasswordCallbackHandler implements CallbackHandler {
+    
+    private Map<String, String> users = new HashMap<String, String>();
+    
+    public UsernamePasswordCallbackHandler() {
+        users.put("wernerd", "verySecret");
+        users.put("bob", "security");
+        users.put("alice", "securityPassword");
+    }
+    
+    public void handle(Callback[] callbacks)
+        throws IOException, UnsupportedCallbackException {
+        for (int i = 0; i < callbacks.length; i++) {
+            if (callbacks[i] instanceof WSPasswordCallback) {
+                WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
+                switch (pc.getUsage()) {
+                case WSPasswordCallback.USERNAME_TOKEN: {
+                    pc.setPassword(users.get(pc.getIdentifier()));
+                    break;
+                }
+                case WSPasswordCallback.USERNAME_TOKEN_UNKNOWN: {
+                    String password = users.get(pc.getIdentifier());
+                    if (password != null) {
+                        pc.setPassword(password);
+                    } else {
+                        throw new IOException("Authentication failed");
+                    }
+                    break;
+                }
+                default:
+                    throw new IOException("Authentication failed");
+                }
+            } else {
+                throw new UnsupportedCallbackException(callbacks[i], "Unrecognized Callback");
+            }
+        }
+    }
+}
