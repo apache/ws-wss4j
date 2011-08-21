a61dc6e4084518f733510b069a00b8c3a6371b56
Marc Giger <giger@apache.org>
2011-08-21 11:10:19 +0000

    correctly set the gpl license on all files

--- streaming-webservice-security/trunk/src/main/java/org/swssf/impl/securityToken/DelegatingSecurityToken.java
+++ streaming-webservice-security/trunk/src/main/java/org/swssf/impl/securityToken/DelegatingSecurityToken.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2010, 2011 Marc Giger
+ *
+ * This file is part of the streaming-webservice-security-framework (swssf).
+ *
+ * The streaming-webservice-security-framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The streaming-webservice-security-framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the streaming-webservice-security-framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.swssf.impl.securityToken;
 
 import org.swssf.ext.Constants;
@@ -9,29 +27,11 @@ import java.security.PublicKey;
 import java.security.cert.X509Certificate;
 
 /**
- * User: giger
- * Date: 5/24/11
- * Time: 6:33 PM
- * Copyright 2011 Marc Giger gigerstyle@gmx.ch
- * <p/>
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2, or (at your option) any
- * later version.
- * <p/>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-
-/**
  * This class wraps a SecurityToken and allows the token KeyIdentifierType to
  * be set differently by its actual usage
+ *
+ * @author $Author$
+ * @version $Revision$ $Date$
  */
 public class DelegatingSecurityToken implements SecurityToken {
 
