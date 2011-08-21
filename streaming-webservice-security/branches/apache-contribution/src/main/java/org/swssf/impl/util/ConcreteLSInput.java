8bd97477c2e65507e4b3193ea40712da1b6f1c14
Marc Giger <giger@apache.org>
2011-08-21 10:41:49 +0000

    apply Apache Software License V2

--- streaming-webservice-security/branches/apache-contribution/src/main/java/org/swssf/impl/util/ConcreteLSInput.java
+++ streaming-webservice-security/branches/apache-contribution/src/main/java/org/swssf/impl/util/ConcreteLSInput.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.swssf.impl.util;
 
 import org.w3c.dom.ls.LSInput;
@@ -6,24 +24,8 @@ import java.io.InputStream;
 import java.io.Reader;
 
 /**
- * User: giger
- * Date: 5/28/11
- * Time: 8:08 PM
- * Copyright 2011 Marc Giger gigerstyle@gmx.ch
- * <p/>
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2, or (at your option) any
- * later version.
- * <p/>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * @author $Author$
+ * @version $Revision$ $Date$
  */
 public class ConcreteLSInput implements LSInput {
 
