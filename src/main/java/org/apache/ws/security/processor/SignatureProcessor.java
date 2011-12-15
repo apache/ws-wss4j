a1dad1f0be6417a9d1857e232bfb235f038bd33c
Colm O hEigeartaigh <coheigea@apache.org>
2011-12-15 16:14:30 +0000

    [WSS-326] - Added in secure validation

--- src/main/java/org/apache/ws/security/processor/SignatureProcessor.java
+++ src/main/java/org/apache/ws/security/processor/SignatureProcessor.java
@@ -356,6 +356,7 @@ public class SignatureProcessor implements Processor {
         
         XMLValidateContext context = new DOMValidateContext(key, elem);
         context.setProperty("javax.xml.crypto.dsig.cacheReference", Boolean.TRUE);
+        context.setProperty("org.apache.jcp.xml.dsig.secureValidation", Boolean.TRUE);
         context.setProperty(STRTransform.TRANSFORM_WS_DOC_INFO, wsDocInfo);
         
         try {
