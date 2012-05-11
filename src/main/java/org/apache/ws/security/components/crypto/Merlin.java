064b03279e94aa79b7a1baf9ac79fe07313256c6
Colm O hEigeartaigh <coheigea@apache.org>
2012-05-11 16:14:21 +0000

    Fix for a NPE when verifying a PublicKey

--- src/main/java/org/apache/ws/security/components/crypto/Merlin.java
+++ src/main/java/org/apache/ws/security/components/crypto/Merlin.java
@@ -1235,6 +1235,9 @@ public class Merlin extends CryptoBase {
      * Find the Public Key in a keystore. 
      */
     private boolean findPublicKeyInKeyStore(PublicKey publicKey, KeyStore keyStoreToSearch) {
+        if (keyStoreToSearch == null) {
+            return false;
+        }
         try {
             for (Enumeration<String> e = keyStoreToSearch.aliases(); e.hasMoreElements();) {
                 String alias = e.nextElement();
