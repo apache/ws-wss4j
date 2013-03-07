de80a0dcf258414e56d35f67d33ec7b5d50ab320
Colm O hEigeartaigh <coheigea@apache.org>
2013-03-07 11:07:09 +0000

    Avoid a NPE in CryptoFactory on a null Properties object

--- src/main/java/org/apache/ws/security/components/crypto/CryptoFactory.java
+++ src/main/java/org/apache/ws/security/components/crypto/CryptoFactory.java
@@ -73,6 +73,12 @@ public abstract class CryptoFactory {
      * @throws WSSecurityException if there is an error in loading the crypto properties
      */
     public static Crypto getInstance(Properties properties) throws WSSecurityException {
+        if (properties == null) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Cannot load Crypto instance as properties object is null");
+            }
+            throw new WSSecurityException("Cannot load Crypto instance as properties object is null");
+        }
         return getInstance(properties, Loader.getClassLoader(CryptoFactory.class));
     }
 
@@ -96,6 +102,13 @@ public abstract class CryptoFactory {
         Properties properties, 
         ClassLoader classLoader
     ) throws WSSecurityException {
+        if (properties == null) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Cannot load Crypto instance as properties object is null");
+            }
+            throw new WSSecurityException("Cannot load Crypto instance as properties object is null");
+        }
+        
         String cryptoClassName = properties.getProperty("org.apache.ws.security.crypto.provider");
         Class<? extends Crypto> cryptoClass = null;
         if (cryptoClassName == null 
