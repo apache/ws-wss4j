de5104b30ddde5fe7388ad57e1c5ace5c5509924
Colm O hEigeartaigh <coheigea@apache.org>
2014-08-29 15:57:44 +0000

    [CVE-2015-0226] Some changes to random key generation

--- ws-security-dom/src/main/java/org/apache/wss4j/dom/processor/EncryptedKeyProcessor.java
+++ ws-security-dom/src/main/java/org/apache/wss4j/dom/processor/EncryptedKeyProcessor.java
@@ -19,6 +19,7 @@
 
 package org.apache.wss4j.dom.processor;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 import java.security.spec.MGF1ParameterSpec;
@@ -244,7 +245,7 @@ public class EncryptedKeyProcessor implements Processor {
     private static byte[] getRandomKey(List<String> dataRefURIs, Document doc, WSDocInfo wsDocInfo) throws WSSecurityException {
         try {
             String alg = "AES";
-            int size = 128;
+            int size = 16;
             if (!dataRefURIs.isEmpty()) {
                 String uri = dataRefURIs.iterator().next();
                 Element ee = ReferenceListProcessor.findEncryptedDataElement(doc, wsDocInfo, uri);
@@ -256,8 +257,16 @@ public class EncryptedKeyProcessor implements Processor {
             kgen.init(size * 8);
             SecretKey k = kgen.generateKey();
             return k.getEncoded();
-        } catch (Exception ex) {
-            throw new WSSecurityException(WSSecurityException.ErrorCode.FAILED_CHECK, ex);
+        } catch (Throwable ex) {
+            // Fallback to just using AES to avoid attacks on EncryptedData algorithms
+            try {
+                KeyGenerator kgen = KeyGenerator.getInstance("AES");
+                kgen.init(128);
+                SecretKey k = kgen.generateKey();
+                return k.getEncoded();
+            } catch (NoSuchAlgorithmException e) {
+                throw new WSSecurityException(WSSecurityException.ErrorCode.FAILED_CHECK, e);
+            }
         }
     }
     
