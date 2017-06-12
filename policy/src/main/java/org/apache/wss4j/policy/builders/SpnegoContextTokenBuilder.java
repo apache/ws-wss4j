0d69a32a52f1c23b4d8feec1d0286afe01f77cac
Colm O hEigeartaigh <coheigea@apache.org>
2017-06-12 13:03:56 +0000

    WSS-608 - Allow policy tokens not to have a wsp:Policy child element

--- policy/src/main/java/org/apache/wss4j/policy/builders/SpnegoContextTokenBuilder.java
+++ policy/src/main/java/org/apache/wss4j/policy/builders/SpnegoContextTokenBuilder.java
@@ -48,9 +48,6 @@ public class SpnegoContextTokenBuilder implements AssertionBuilder<Element> {
         
         Policy nestedPolicy = null;
         if (nestedPolicyElement == null) {
-            if (spVersion != SPConstants.SPVersion.SP11) {
-                throw new IllegalArgumentException("sp:SpnegoContextToken must have an inner wsp:Policy element");
-            }
             nestedPolicy = new Policy();
         } else {
             nestedPolicy = factory.getPolicyEngine().getPolicy(nestedPolicyElement);
