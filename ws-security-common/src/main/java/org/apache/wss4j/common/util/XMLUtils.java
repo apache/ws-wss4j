462c23a0cb75f89484dc060249a3875d94c75bf9
Gary Gregory <garydgregory@gmail.com>
2026-09-07 13:09:10 -0400

    Harden XML parsing via commons-secure-xml
    Remove now redundant code.

--- ws-security-common/src/main/java/org/apache/wss4j/common/util/XMLUtils.java
+++ ws-security-common/src/main/java/org/apache/wss4j/common/util/XMLUtils.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import javax.xml.XMLConstants;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -142,16 +141,7 @@ public final class XMLUtils {
         throws TransformerException {
         DOMSource source = new DOMSource(element);
         StreamResult result = new StreamResult(out);
-
         TransformerFactory transFactory = SecureTransformerFactory.newInstance();
-        transFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        try {
-            transFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-            transFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
-        } catch (IllegalArgumentException ex) { //NOPMD
-            // ignore
-        }
-
         Transformer transformer = transFactory.newTransformer();
         transformer.transform(source, result);
     }
