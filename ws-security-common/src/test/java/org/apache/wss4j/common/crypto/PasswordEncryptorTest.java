b1f77e8da6ad2a9a37fc587747f46093201c298e
Colm O hEigeartaigh <coheigea@users.noreply.github.com>
2026-09-10 06:58:58 +0100

    WSS-729 Switch default JasyptPasswordEncryptor algorithm to PBEWithHmacSHA512AndAES_256 (#683)

--- ws-security-common/src/test/java/org/apache/wss4j/common/crypto/PasswordEncryptorTest.java
+++ ws-security-common/src/test/java/org/apache/wss4j/common/crypto/PasswordEncryptorTest.java
@@ -40,4 +40,41 @@ public class PasswordEncryptorTest {
         assertEquals(decryptedPassword, "password");
     }
 
+    @Test
+    public void testJasyptDefaultAlgorithm() throws Exception {
+        // The strong algorithm is the default on both the FIPS and non-FIPS paths, and
+        // round-trips without needing any pre-encrypted fixture
+        assertEquals(JasyptPasswordEncryptor.DEFAULT_ALGORITHM, "PBEWithHmacSHA512AndAES_256");
+
+        PasswordEncryptor passwordEncryptor =
+            new JasyptPasswordEncryptor("master-password");
+        String encryptedPassword = passwordEncryptor.encrypt("password");
+        assertNotEquals(encryptedPassword, "password");
+        String decryptedPassword = passwordEncryptor.decrypt(encryptedPassword);
+        assertEquals(decryptedPassword, "password");
+    }
+
+    @Test
+    public void testJasyptLegacyDefaultAlgorithmOptIn() throws Exception {
+        // A value encrypted under the previous default algorithm...
+        PasswordEncryptor legacyEncryptor =
+            new JasyptPasswordEncryptor("master-password",
+                                        JasyptPasswordEncryptor.LEGACY_DEFAULT_ALGORITHM);
+        String legacyEncryptedPassword = legacyEncryptor.encrypt("password");
+        assertNotEquals(legacyEncryptedPassword, "password");
+
+        // ...can still be decrypted with the default constructor by opting in via the system
+        // property. The class is already loaded at this point, so this also checks that the
+        // property is honored at construction time rather than at class-loading time.
+        try {
+            System.setProperty(JasyptPasswordEncryptor.USE_LEGACY_DEFAULT_ALGORITHM_PROPERTY, "true");
+            PasswordEncryptor passwordEncryptor =
+                new JasyptPasswordEncryptor("master-password");
+            String decryptedPassword = passwordEncryptor.decrypt(legacyEncryptedPassword);
+            assertEquals(decryptedPassword, "password");
+        } finally {
+            System.clearProperty(JasyptPasswordEncryptor.USE_LEGACY_DEFAULT_ALGORITHM_PROPERTY);
+        }
+    }
+
 }
--- ws-security-stax/src/test/java/org/apache/wss4j/stax/test/PasswordEncryptorTest.java
+++ ws-security-stax/src/test/java/org/apache/wss4j/stax/test/PasswordEncryptorTest.java
@@ -41,6 +41,8 @@ import org.apache.wss4j.stax.setup.OutboundWSSec;
 import org.apache.wss4j.stax.setup.WSSec;
 import org.apache.wss4j.stax.test.utils.XmlReaderToWriter;
 import org.apache.xml.security.stax.securityEvent.SecurityEvent;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -57,6 +59,18 @@ import static org.junit.jupiter.api.Assertions.fail;
  * password
  */
 public class PasswordEncryptorTest extends AbstractTestBase {
+
+    // The ENC() value in transmitter-crypto-enc.properties was encrypted under the previous
+    // default algorithm (PBEWithMD5AndTripleDES), so opt in to the legacy default for this class
+    @BeforeAll
+    public static void setUpLegacyDefaultAlgorithm() {
+        System.setProperty(JasyptPasswordEncryptor.USE_LEGACY_DEFAULT_ALGORITHM_PROPERTY, "true");
+    }
+
+    @AfterAll
+    public static void tearDownLegacyDefaultAlgorithm() {
+        System.clearProperty(JasyptPasswordEncryptor.USE_LEGACY_DEFAULT_ALGORITHM_PROPERTY);
+    }
 
     @Test
     public void testSignatureCryptoPropertiesOutbound() throws Exception {
